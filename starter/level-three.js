6447b32eb1934611ce93a7d1f93c04ec4d297da0
jharreldesign <harrel.jason@gmail.com>
2024-09-24 15:26:48 -0700

    add functionality by clicking balls on each level to get points. Add a removeEventListener to not add more points by the player target score is reached.

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,24 @@
  * DOTS: Level Three
  *
  */
+
+let score = 0;
+
+const balls = document.querySelectorAll('.js-ball');
+const scoreDisplay = document.querySelector('.js-score')
+const levelWinner = document.querySelector('.level-winner')
+
+for (i = 0; i < balls.length; i++) {
+    let scoreValue = parseInt(balls[i].dataset.increment)
+    balls[i].addEventListener('click', () => {
+        score += scoreValue;
+        scoreDisplay.textContent = score;
+        if(score >= 100) {
+            levelWinner.style.opacity = 1;
+            // Remove event listener so you can't click on the balls/add points after hitting 100 point.
+            balls[i].removeEventListener('click', handleClick);
+        }
+    })
+}
+
+
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,24 @@
  * DOTS: Level Two
  *
  */
+
+
+const balls = document.querySelectorAll('.js-ball');
+const scoreElement = document.querySelector('.js-score');
+const winnerSection = document.querySelector('.level-winner');
+let score = 0;
+
+console.log(balls)
+
+for (let i = 0; i < balls.length; i++) {
+    balls[i].addEventListener('click', function() {
+        score += 10;
+        scoreElement.textContent = `Score: ${score}`;
+
+        if (score >= 100) {
+            winnerSection.style.opacity = 1;
+            // Remove event listener
+            balls[i].removeEventListener('click', handleClick);
+        }
+    })
+}
